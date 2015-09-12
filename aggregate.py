0abe047078ee073489673a28b677b2fe94beb5b1
Joshua Davis <josh314@gmail.com>
2015-09-12 19:24:22 -0400

    Some doc string editing

--- aggregate.py
+++ aggregate.py
@@ -10,25 +10,26 @@ if __name__ == '__main__' and len(sys.argv) < 2:
 import pandas as pd
 import numpy as np
 
-def skaters_total_scoring(dat):
+def skaters_total_scoring(events):
     pass
 
-def init_goalie_table(dat):
+def _init_goalie_table(events):
     # dropna() removes nan from empty net situations
-    def uniq_col(x): return set(dat[x].dropna().unique()) 
+    def uniq_col(x): return set(events[x].dropna().unique()) 
     goalies = sorted( list( uniq_col('away.G').union( uniq_col('home.G') ) ) )
     return pd.DataFrame(index=goalies)
 
-def aggregate_goals_against(dat):
-    goals = dat[dat['etype']=='GOAL']
+def aggregate_goals_against(events):
+    "Aggregate goals against for individual goalies over the input set of events. Returns a series indexed by goalie text_id."
+    goals = events[events['etype']=='GOAL']
     goals_against_goalie = pd.Series(data=goals['home.G'],index=goals.index)
     goals_against_goalie[goals['ev.team']==goals['hometeam']] = goals['away.G']
     return goals_against_goalie.dropna().value_counts().sort_index()
     
-def goalies_all_stats(dat):
-    "Individual goalie data aggregated over all games and game situations"
-    goalies = init_goalie_table(dat)
-    goalies.insert(0,'GA',aggregate_goals_against(dat))
+def goalies_all_stats(events):
+    "Individual goalie data aggregated over the input set of events"
+    goalies = _init_goalie_table(events)
+    goalies.insert(0,'GA',aggregate_goals_against(events))
     return goalies
     
     
@@ -41,9 +42,9 @@ def _set_io_files():
 
 def _main():
     infile, outfile = _set_io_files()
-    dat = pd.read_csv(infile,header=0)
+    events = pd.read_csv(infile,header=0)
 
-    #out = full_aggregation(dat)
+    #out = full_aggregation(events)
     out.to_csv(outfile,index=False)
 
 if __name__ == '__main__':
