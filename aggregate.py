900948ca55e931546ded537f1c15d2a7ebad8148
Joshua Davis <josh314@gmail.com>
2015-09-14 14:15:18 -0400

    Finished first run on game-winning-goal function. Still need to test

--- aggregate.py
+++ aggregate.py
@@ -1,16 +1,19 @@
 """
-Perform aggragation on the cleaned WAR-On-Ice data.
+Perform aggregation on the cleaned WAR-On-Ice data. 
 """
 import sys
 
 if __name__ == '__main__' and len(sys.argv) < 2:
     sys.exit("Usage: aggregate <input CSV> <optional output CSV>")
 
-    
 import pandas as pd
 import numpy as np
 
-def skaters_total_scoring(events):
+### Magic numbers #########
+_playoff_gcode_start = 30000
+###########################
+
+def skaters_total_scoring(events):#TODO
     pass
 
 def _goalie_index(events):
@@ -19,10 +22,25 @@ def _goalie_index(events):
     goalies = sorted( list( uniq_col('away.G').union(uniq_col('home.G')) ) )
     return pd.Index(goalies)
 
-def goalies_games_played(events):
+def goalies_games_played(events):#TODO
     games = events['gcode'].unique()
     for game in games:
         pass
+
+def game_winning_goals(events):
+    "Returns the subset of the input events which represent game winning goals. Shootout goals should be filtered out of input."
+    goals = events[events['etype']=='GOAL']
+    games = goals.groupby('gcode')
+    return games.apply(_find_gwg)
+    
+def _find_gwg(game_goals):#Returns empty df if a tie
+    def _max_score(team):#team = 'home' or 'away'
+        return game_goals[team+'.score'].max()
+    winner, loser = 'home', 'away'
+    if _max_score('away') > _max_score('home'):
+        winner, loser = 'away', 'home'
+    gwg = game_goals[game_goals[winner+'.score'] > _max_score(loser)].head(1)
+    return gwg
 
 def goalies_games_started(events,goalie_index=None):
     "Aggregate goalie starts over input events. A list or index of goalies can be input if already calculated; otherwise computed on the fly from events. Returns data frame with columns: (starts,home_starts,away_starts)."
@@ -49,7 +67,7 @@ def saves(events):
     goalie[saved_shots['ev.team']==saved_shots['hometeam']] = saved_shots['away.G']
     return goalie.dropna().value_counts().sort_index()
 
-def goalies_aggregation_stats(events):
+def goalies_aggregation_stats(events):#WIP
     "Individual goalie data aggregated over the input events"
     goalies = pd.DataFrame(index=_goalie_index(events))
     goalies.insert(len(goalies.columns),'goals_against',goals_against(events))
@@ -65,13 +83,15 @@ def _set_io_files():
         outfile = sys.argv[2]
     return (infile,outfile)
 
-def _main():
+def _main():#WIP
     infile, outfile = _set_io_files()
     events = pd.read_csv(infile,header=0)
-
+    #goalies = goalie_stats()
+    #forwards = forward_stats()
+    #dmen = dmen_stats()
     #out = full_aggregation(events)
     out.to_csv(outfile,index=False)
 
-if __name__ == '__main__':
+if __name__ == '__main__':#WIP
     #_main()
     pass
